933d875b938a0112aa40dc4704595ce399306465
nameless kid <107755554+sojupatil@users.noreply.github.com>
2023-05-02 10:22:34 +0530

    New ~~

--- js/fetch.js
+++ js/fetch.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
-	$.ajax({
-		url: "from/menu.html",
-		success: function (response) {
-			$("#menu").html(response);
-		}
-	});
+  $.ajax({
+    url: "from/menu.html",
+    success: function (response) {
+      $("#menu").html(response);
+    },
   });
-  
+});
+
+$(document).ready(function () {
+  $.ajax({
+    url: "from/story.html",
+    success: function (response) {
+      $("#story").html(response);
+    },
+  });
+});
+
+$(document).ready(function () {
+  $.ajax({
+    url: "from/maincontent.html",
+    success: function (response) {
+      $("#maincontent").html(response);
+    },
+  });
+});
